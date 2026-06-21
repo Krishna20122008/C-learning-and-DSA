d2f65979db469a8f94cbba8bb2fcd19bc3483928
Krishna20122008 <kchourasia121@gmail.com>
2026-06-21 12:10:48 +0530

    Day-02: Pushed AreaOfCircle.cpp

---
+++ AreaOfCircle.cpp
@@ -0,0 +1,12 @@
+// Area of circle in C++
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    float radius;
+    cin >> radius;
+    float area = 3.14 * radius * radius;
+    cout << "The Area of Circle is: " <<area << endl;
+
+}
