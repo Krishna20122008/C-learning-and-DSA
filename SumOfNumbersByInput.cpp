82fa6e87d407cf3b9810e95632422f33ce1f1eb3
Krishna20122008 <kchourasia121@gmail.com>
2026-06-21 22:35:52 +0530

    Day

---
+++ SumOfNumbersByInput.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
+    
+    int y;
+    cout << "Enter a number: ";
+    cin >> y;
+
+    int sum = x+y;
+    cout << "Sum of the entered digits is: " << sum <<endl;
+}
