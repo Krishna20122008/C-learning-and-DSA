8d6067d742132e977c5b631175fb795af26d31c6
Krishna20122008 <kchourasia121@gmail.com>
2026-06-22 22:47:54 +0530

    day

---
+++ PostPreIncrementDecrementOperators.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int x = 10;
+    cout << x << endl;
+
+    x++ ;    // x++ is same as x = x+1
+    cout << x << endl;
+
+    ++x ;    // ++x is same as x = x+1
+    cout << x << endl;
+
+    x-- ;    // x-- is same as x = x-1
+    cout << x << endl;
+
+    --x ;    // x-- is same as x = x-1
+    cout << x << endl;
+
+    int y =10;
+    cout << y << endl;
+    cout << y++ << endl;    // here, its postfix so, after y is printed, then addition is done.
+    cout << ++y << endl;    // here, its prefix so, before y is printed, addition is done earlier.
+}
