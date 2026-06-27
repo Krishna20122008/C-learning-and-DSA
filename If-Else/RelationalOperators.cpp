3f8ff537a34b4df8f20dc2f01d87bdb35224566d
Krishna20122008 <kchourasia121@gmail.com>
2026-06-27 19:17:29 +0530

    Pushed Boolean and Relational Operators basics

---
+++ If-Else/RelationalOperators.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << (5>3) << endl;
+    cout << (5<3) << endl;
+    cout << (8 == 8) << endl;
+    cout << (8 == 6) << endl;
+    cout << (8 >= 6) << endl;
+    cout << (8 != 6) << endl;
+    bool a = (5 != 5);
+    cout << a << endl;
+    
+}
