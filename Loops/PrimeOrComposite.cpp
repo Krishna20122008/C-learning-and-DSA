f9c01a264c366b1e736aceb9a60adc817355b165
Krishna20122008 <kchourasia121@gmail.com>
2026-07-05 20:03:27 +0530

    Pushed PrimeOrComposite.cpp

---
+++ Loops/PrimeOrComposite.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int count = 0;
+    for (int i=1; i<=sqrt(n); i++){
+        if (n%i==0){ count++ ;
+        }
+    }
+    if (count>=3) cout << "Composite Number" << endl;
+    else cout << "Prime Number" << endl;    
+}
