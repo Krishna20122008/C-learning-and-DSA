76b72da602d5f94b7a6667c0d5b61598c87f56d6
Krishna20122008 <kchourasia121@gmail.com>
2026-07-05 11:35:10 +0530

    Pushed TableOf19.cpp

---
+++ Loops/TableOf19.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Table of 19:" << endl;
+    for (int i=1; i<=10; i++){
+        cout << "19 X " << i << " = " << i*19 << endl;
+    }
+}
