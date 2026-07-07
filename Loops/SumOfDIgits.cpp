fb2f8a98af78abedf2dc160f78c3718a72de573d
Krishna20122008 <kchourasia121@gmail.com>
2026-07-07 10:38:23 +0530

    Pushed SumOfDigits.cpp

---
+++ Loops/SumOfDIgits.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int sum = 0;
+    int n;
+    cout << "Enter a number: " ;
+    cin >> n;
+
+    while (n!=0){
+        sum+=(n%10);
+        n/=10;
+    }
+    cout << "Sum of digits is: " << sum << endl;
+}
