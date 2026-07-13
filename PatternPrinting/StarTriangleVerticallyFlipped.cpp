fd0a46fb4461f87549c609530574e588bcd7e96e
Krishna20122008 <kchourasia121@gmail.com>
2026-07-13 19:55:21 +0530

    Pushed StartriangleVerticallyFlipped.cpp

---
+++ PatternPrinting/StarTriangleVerticallyFlipped.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a;
+    cout << "Enter No. of rows: ";
+    cin >> a;
+
+    for (int i=1; i<=a; i++){
+        for (int j=1; j<=a-i; j++){       // j<=a+1-i ki jagah a-i kiya hai because output me ek space aa ja rha tha front me
+            cout << "  " ;
+        }
+        for (int j=1; j<=i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
