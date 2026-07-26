ed0b63521731ba462f49b9c7da1f2d1a814aa255
Krishna20122008 <kchourasia121@gmail.com>
2026-07-26 22:31:14 +0530

    Pushed OutputArray.cpp, GarbageValue.cpp, ArrayInput.cpp

---
+++ Arrays/GarbageValues.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[4];
+    cout << arr[2] << endl; // No value is currently assigned by user to the index 2 of the array but some default values called garbage values are already there in those indices..
+}
---
+++ Arrays/OutputArray.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {23,56,656,32,67,23,55};
+    cout << "The elements of the array are: ";
+    for (int i = 0; i<size(arr); i++){
+        cout << arr[i] << " ";
+    }
+}
