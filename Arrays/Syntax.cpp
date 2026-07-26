20400589d64bdbf32b8c0dfad04ebe4f2c314d68
Krishna20122008 <kchourasia121@gmail.com>
2026-07-26 22:06:37 +0530

    Pushed syntax.cpp

--- Arrays/Syntax.cpp
+++ Arrays/Syntax.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(){
     int marks[] = {32,45,33,67,34}; // Array
     cout << marks[4] << endl;
+    cout << size(marks) << endl;
     cout << marks[2] << endl;
     marks[2] = 54;
     cout << marks[2] << endl;
