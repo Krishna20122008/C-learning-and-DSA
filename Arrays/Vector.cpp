0cbdd7c88f1494063cb009d4c61bed25c1c18b52
Krishna20122008 <kchourasia121@gmail.com>
2026-08-01 22:44:41 +0530

    Pushed Vector.cpp

---
+++ Arrays/Vector.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> arr(5);
+    arr[0] = 54;
+    for (int i=0; i<5; i++){
+        cout << arr[i] << " ";
+    }
+}
