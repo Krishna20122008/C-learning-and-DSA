6caf1ebf0e22fc4cff3c1b32917a23bab481cf80
Krishna20122008 <kchourasia121@gmail.com>
2026-08-01 23:12:48 +0530

    Edited vector.cpp

--- Arrays/Vector.cpp
+++ Arrays/Vector.cpp
@@ -7,4 +7,12 @@ int main(){
     for (int i=0; i<5; i++){
         cout << arr[i] << " ";
     }
+    cout << endl;
+
+    vector<int> grr(5, 3);      // in vectors (also called dynamic arrays), if we write any number after the size of array, the value is set to default value
+    grr[0] = 43;
+    for (int i=0; i<5; i++){
+        cout << grr[i] << " ";
+    }
+    cout << endl;
 }
