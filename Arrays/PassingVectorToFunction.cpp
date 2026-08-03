fb6cacce2a6a567c73e4eb1f076230735b634d8d
Krishna20122008 <kchourasia121@gmail.com>
2026-08-03 09:44:14 +0530

    Edited PassingVectorToFunction.cpp

--- Arrays/PassingVectorToFunction.cpp
+++ Arrays/PassingVectorToFunction.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int change(vector<int> v){      // vector is pass by value and array is pass by reference
     v[2] = 45;                  // so the value of array actually changes but vector value doesn't change
 }
+
+int change1(vector<int>& v){      // now when we type & before the name of vector, it becomes pass by reference and the value changes
+    v[2] = 45;
+}
 int main(){
     vector<int> arr = {34,5,2,53,51};
     cout << "original: ";
@@ -12,6 +16,11 @@ int main(){
 
     change(arr);
     cout << "new: " ;
+    for(int ele : arr) cout << ele << " ";
+    cout << endl;
+
+    change1(arr);
+    cout << "newest: " ;
     for(int ele : arr) cout << ele << " ";
     cout << endl;
 }
