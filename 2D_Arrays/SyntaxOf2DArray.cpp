5e9c932a716ad5cf59b55f2900c2d564f414ecdb
Krishna20122008 <kchourasia121@gmail.com>
2026-08-05 22:33:01 +0530

    Edited SyntaxOf2DArray.cpp

--- 2D_Arrays/SyntaxOf2DArray.cpp
+++ 2D_Arrays/SyntaxOf2DArray.cpp
@@ -4,7 +4,16 @@ int main(){
     int arr[][4] = {{2,4,5,2}, {4,6,3,4}, {5,7,2,1}};
     for (int i=0; i<3; i++){
         for(int j=0; j<4; j++){
-            cout << arr[i][j] << " ";
+            cout << arr[i][j] << " ";       // Its like printing a matrix
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    int grr[][4] = {{2,4,5,2}, {4,6,3,4}, {5,7,2,1}};
+    for (int j=0; j<4; j++){
+        for(int i=0; i<3; i++){
+            cout << arr[i][j] << " ";       // Its like printing the transpose of the matrix
         }
         cout << endl;
     }
